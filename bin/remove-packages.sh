c2a08d54ea2a913737a3918bb9080b9deb4305a6
Andrew Rea <email@andrewrea.co.uk>
2017-09-26 05:58:06 +0000

    Removing non-required packages and basing each base image on the previous one

---
+++ bin/remove-packages.sh
@@ -0,0 +1,17 @@
+# Remove
+sudo yum -y remove xinetd
+sudo yum -y remove telnet-server
+sudo yum -y remove rsh-server
+sudo yum -y remove telnet
+sudo yum -y remove rsh-server
+sudo yum -y remove rsh
+sudo yum -y remove ypbind
+sudo yum -y remove ypserv
+sudo yum -y remove tftp-server
+sudo yum -y remove cronie-anacron
+sudo yum -y remove bind
+sudo yum -y remove vsftpd
+sudo yum -y remove httpd
+sudo yum -y remove dovecot
+sudo yum -y remove squid
+sudo yum -y remove net-snmpd
